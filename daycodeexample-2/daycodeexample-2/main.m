a42e138b6c027bc2f1372ff6a368e680dfd93d41
sampathreddy432 <reddysampath425@gmail.com>
2014-05-06 15:56:48 -0400

    day2
    fractions

--- daycodeexample-2/daycodeexample-2/main.m
+++ daycodeexample-2/daycodeexample-2/main.m
@@ -7,15 +7,50 @@
 //
 
 #import <Foundation/Foundation.h>
+// @interface section
+@interface Fraction : NSObject
+-(void)print;
+-(void)setNumerator: (int)n;
+-(void)setDenominator: (int)d;
 
+@end
+// @implementation section
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+-(void)print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+-(void)setNumerator:(int)n
+{
+    numerator = n;
+}
+-(void)setDenominator:(int)d
+{
+    denominator =d;
+}
+@end
+// program section
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        int numerator = 1;
-        int denominator = 3;
-        NSLog(@"the fraction is %i/%i", numerator, denominator);
+        Fraction *myFraction;
+        //create an instance of a Fraction
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        //set the fraction
+        [myFraction setNumerator:1];
+        [myFraction setDenominator:3];
+        // display the fraction using out print method
+        NSLog(@"the value of myFraction is:");
+        [myFraction print];
+        
         
     }
     return 0;
