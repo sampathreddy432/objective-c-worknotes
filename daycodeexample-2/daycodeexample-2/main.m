5fa0f29718329eb82eb63afa26c69cec9e114ef8
sampathreddy432 <reddysampath425@gmail.com>
2014-05-06 13:53:53 -0400

    starter project
    hello world example

---
+++ daycodeexample-2/daycodeexample-2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  daycodeexample-2
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 sampath reddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
