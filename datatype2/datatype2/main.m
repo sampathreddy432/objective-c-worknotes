147db7fc67ca28f69ddc2aad4382e0063dd7beb8
sampathreddy <reddysampath425@gmail.com>
2014-05-07 14:59:10 -0400

    operators
    code1

---
+++ datatype2/datatype2/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  datatype2
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 sampathreddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        
+    }
+    return 0;
+}
+
+￼
