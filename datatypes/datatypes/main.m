7f292466f4fe8c59f0edd9deb37b7c59dc49f2db
sampathreddy <reddysampath425@gmail.com>
2014-05-07 14:28:44 -0400

    Initial Commit

---
+++ datatypes/datatypes/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  datatypes
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 sampathreddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
