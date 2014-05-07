a8873247b83a22a5d06baff692a1dcae2d5f61b8
sampathreddy <reddysampath425@gmail.com>
2014-05-07 16:00:03 -0400

    character
    code character

---
+++ character/character/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  character
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 sampathreddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        char c,d;
+        c = 'd';
+        d = c;
+        NSLog(@"d = %c", d);
+        
+    }
+    return 0;
+}
+
