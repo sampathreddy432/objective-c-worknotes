052083946974214d728f233a9ca5f3c5c0278285
sampathreddy <reddysampath425@gmail.com>
2014-05-09 14:05:57 -0400

    ifexample2
    code

---
+++ if example 1/if example 1/main.m
@@ -0,0 +1,73 @@
+//
+//  main.m
+//  if example 1
+//
+//  Created by User1 on 2014-05-09.
+//  Copyright (c) 2014 sampathreddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface Fraction: NSObject
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+-(int) numerator;
+-(int) denominator;
+-(double) convertToNum;
+@end
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+-(void) setNumerator:(int) n
+{
+    numerator = n;
+}
+-(void) setDenominator:(int) d
+{
+    denominator = d;
+}
+-(int) numerator
+{
+    return numerator;
+}
+-(int) denominator
+{
+    return denominator;
+}
+-(double) convertToNum
+{
+    if(denominator !=0){
+        return (double) numerator / denominator;
+    }
+        else
+            return NAN;
+}
+@end
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        Fraction *aFraction = [[Fraction alloc] init];
+        Fraction *bFraction = [[Fraction alloc] init];
+        [aFraction setNumerator: 1];
+        [aFraction setDenominator: 4];
+        [aFraction print];
+        NSLog (@" =");
+        NSLog (@"%g", [aFraction convertToNum]);
+        
+        [bFraction print];
+        NSLog(@" =");
+        NSLog (@"%g", [bFraction convertToNum]);
+    }
+    return 0;
+}
+
